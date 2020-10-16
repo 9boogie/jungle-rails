bbd4e0816a43be8ca2b5f147c8d65cd83708f168
JaeyoungKim <jzero.youngs89@gmail.com>
2020-10-16 01:07:35 +0000

    something is wrong

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,16 @@ Rails.application.routes.draw do
 
   get '/about' => "about#show"
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+  resources :user
+  resources :sessions
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
